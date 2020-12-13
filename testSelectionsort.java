6edd30fa970367a1b701e3db8bde6e2011490371
Basmalah <balgahmi@umich.edu>
2020-12-13 12:56:42 -0500

    Pass test2

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -12,47 +12,22 @@ import org.junit.Test;
 public class testSelectionSort {
 	@Test
 	public void test() {
-		testPositive();
-		testNegative();
 		testMixed();
 		testDuplicates();
 	}
 
     public testSelectionSort() {
     }
-    
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
-        
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
-    }
-    
-    public void testNegative(){
-        
-        /** Test data contains negative values only **/
-    }
-    
-    public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
-    }
-    
-    public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
-    }
+		public void testMixed(){
 
+			 /** Test data contains with both positive, negative and zeros **/
+		 assertArrayEquals(new int[] {-2,-1,0,1,2},new SelectionSort().basicSelectionSort(new int[] {-1,0,2,-2,1}));
+	 }
+
+	 public void testDuplicates(){
+
+			 /** Test data contains duplicates **/
+		 assertArrayEquals(new int[] {-1,-1,0,2,2},new SelectionSort().basicSelectionSort(new int[] {-1,0,2,-1,2}));
+	 }
 
 }
