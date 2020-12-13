f336e97b07783f2d7d388ac1a681556f47df4d20
Basmalah <balgahmi@umich.edu>
2020-12-13 12:55:34 -0500

    Pass test +,-

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -14,45 +14,37 @@ public class testSelectionSort {
 	public void test() {
 		testPositive();
 		testNegative();
-		testMixed();
-		testDuplicates();
 	}
 
     public testSelectionSort() {
     }
-    
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
-        
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
-    }
-    
-    public void testNegative(){
-        
-        /** Test data contains negative values only **/
-    }
-    
-    public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
-    }
-    
-    public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
-    }
 
+
+	    public void testPositive(){
+	        int[] arr = new int[5];
+	        arr[0] = 8;
+	        arr[1] = 9;
+	        arr[2] = 7;
+	        arr[3] = 10;
+	        arr[4] = 2;
+
+	        int[] Sortedarr = new int[5];
+	        Sortedarr[0] = 2;
+	        Sortedarr[1] = 7;
+	        Sortedarr[2] = 8;
+	        Sortedarr[3] = 9;
+	        Sortedarr[4] = 10;
+
+	        SelectionSort temp=new SelectionSort();
+	        assertArrayEquals(Sortedarr,temp.basicSelectionSort(arr));
+
+	        /** add tests to check for this unit test **/
+	    }
+
+	    public void testNegative(){
+
+	        /** Test data contains negative values only **/
+	    	assertArrayEquals(new int[] {-10,-8,-7,-5,-1},new SelectionSort().basicSelectionSort(new int[] {-1,-8,-10,-7,-5}));
+	    }
 
 }
